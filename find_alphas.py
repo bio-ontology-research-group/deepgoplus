abdb963a06fc7195251c7200ff2f713061f3e13e
Fernando Zhapa <ferynando7@gmail.com>
2020-12-27 12:08:01 +0300

    find alphas

--- find_alphas.py
+++ find_alphas.py
@@ -114,6 +114,8 @@ def main(train_data_file, test_data_file, terms_file,
     with open(last_release_metadata, 'r') as f:
             last_release_data = json.load(f)
             last_release_data['alphas'][ont] = find_alpha(ont, test_df, blast_preds, go_rels, terms)
+            json.dump(last_release_data, f)
+
 
         
 
